59851762491f18dfca9b24d0c3b39243bff9624f
Nikita Ermakov <100200151+agr0meow@users.noreply.github.com>
2023-04-12 16:53:49 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <Stack gap="40px" alignItems="center" px="40px" pt="24px">
         <img src={Logo} alt="logo" width="200px" height="40px" />
         <Typography variant="h5" pb="40px" mt="20px">
-          Made witch ❤️ agr0meow
+          Made with ❤️ agr0meow
         </Typography>
       </Stack>
     </Box>
